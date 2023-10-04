2d654dedb1ed08e2321360e6c37c7af97633b6c9
sunghyunahn <skd@yonsei.ac.kr>
2023-10-04 18:51:12 +0900

    remove some files

--- Frame-Prediction/run_eval.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -J frame_prediction
-#SBATCH -o ./run_eval.txt
-
-echo "### START DATE=$(date)"
-python eval.py --dataset=ped2 --trained_model=best_model_ped2
-echo "### END DATE=$(date)"
